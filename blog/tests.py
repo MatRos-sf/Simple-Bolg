fc377fd05e2dba805669d3b133c57ba73b270083
MatRos-sf <mateusz-rosenkranz@wp.pl>
2022-03-29 13:33:46 +0200

    test

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+from blog.models import Post
+
+
+class SimpleTestObject(TestCase):
+    def setUp(self):
+        Post.objects.create(category='news', title='TEST',
+                            body="TEST,TEST", tags="test")
+        Post.objects.create(title='TEST2', body="ala", tags="test")
+    def test_count_objects(self):
+        count = Post.objects.count()
+        self.assertEqual(count, 2)
+
+
+
